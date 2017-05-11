a8e98fa35daa228cee1501f2354b4f8ce5267355
aoliaopaopao <619193774@qq.com>
2017-05-11 13:46:43 +0800

    setup --version1.0

---
+++ setup/setup.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+#安装常用工具
+
+#文件位置
+dir=/root/setup
+
+rpm -ivh epel-release-6-8.noarch.rpm
+yum install -y ntsysv wget vim openssh-clients java-1.8.0-openjdk* 
+
+#修改network
+cat >> /etc/sysconfig/network << EOF
+NOZEROCONF=yes
+NETWORKING_IPV6=no
+EOF
+
+service network restart
+
+#修改fstab
+sed -i 's:/dev/shm:/tmp:' /etc/fstab
+sed -i '9s/defaults/defaults,nodiratime,noatime/' /etc/fstab
+
+#安装htpdate
+rpm -ivh rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+yum install -y htpdate
+sed -i '$ i SERVERS="www.qq.com www.baidu.com"' /etc/sysconfig/htpdate
+sed -i 's/SERVERS="www.linux.org www.freebsd.org"/SERVERS="www.qq.com www.baidu.com"/' /etc/rc.d/init.d/htpdate
+chkconfig --level 3 htpdate on
+service htpdate start
+
+#修改SELINUX
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+
+#修改tty
+sed -i 's/1-6/1-1/' /etc/sysconfig/init
+
+#修改中文
+sed -i '1d ' /etc/sysconfig/i18n && sed -i '1 i LANG="zh_CN.UTF-8"' /etc/sysconfig/i18n
+
+cat >> /etc/profile <<EOF
+LC_MESSAGES=en_US.UTF-8
+LC_TIME=en_US.UTF-8
+export LC_MESSAGES LC_TIME
+EOF
+
+sed -i 's/inet_protocols = all/inet_protocols = ipv4/' /etc/postfix/main.cf
+
+#修改限制
+cat >> /etc/security/limits.conf << EOF
+* soft nproc  65535
+* hard nproc  65535
+* soft nofile 65535
+* hard nofile 65535
+EOF
+
+sed -i 's/1024/65535/' /etc/security/limits.d/90-nproc.conf
+
+#减少swap分区
+cat >> /etc/sysctl.conf  << EOF
+vm.swappiness = 0
+EOF
+
+sysctl -p
+
+#修改iptables
+sed -i '10 a -A INPUT -p tcp -m multiport --dports 2222 -j ACCEPT' /etc/sysconfig/iptables
+service iptables restart
+
+yum -y update
+
+#编译工具安装
+yum install -y crul curl-devel zlib-devel openssl-devel perl cpio expat-devel gettext-devel gcc perl-ExtUtils-MakeMaker  gcc make gcc-c++ 
+
+#git安装
+#wget https://www.kernel.org/pub/software/scm/git/git-2.11.1.tar.gz
+tar -xvf git-2.11.1.tar.gz
+cd git-2.11.1
+autoconf
+./configure
+make
+make install
+echo "export PATH=$PATH:/usr/local/git/bin" >> /etc/bashrc
+source /etc/bashrc
+
+#apache-maven
+#wget http://apache.fayea.com/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+cd $dir
+tar -xvf apache-maven-3.3.9-bin.tar.gz
+mv apache-maven-3.3.9 /usr/local/src/
+
+echo "MAVEN_HOME=/usr/local/src/apache-maven-3.3.9" >> /etc/profile
+echo "export MAVEN_HOME" >> /etc/profile
+echo 'export PATH=${PATH}:${MAVEN_HOME}/bin' >> /etc/profile
+
+source /etc/profile
+
+
+\cp -f settings.xml /usr/local/src/apache-maven-3.3.9/conf/
+
+#nodejs&&pm2
+#wget https://rpm.nodesource.com/setup_6.x 
+cd $dir
+chmod +x setup_6.x
+./setup_6.x
+yum -y install nodejs
+
+npm install -g pm2
+
+#修改yum
+yum install -y yum-cron
+sed -i 's/plugins=1/plugins=0/' /etc/yum.conf && sed -i 's/enabled = 1/enabled = 0/' /etc/yum.repos.d/rpmforge.repo && sed -i '12 a exclude=kernel* centos-release*' /etc/yum.conf
+chkconfig yum-cron on
+service yum-cron start
---
+++ setup/web_setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo "是否安装mysql,如果执行输入y,否则输入任意字符串"
+read judge1
+if [ "$judge1" = "y" ] ;then
+	echo "正在安装mysql"
+	rpm -ivh http://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm
+	yum install -y mysql-server mysql mysql-deve
+	chkconfig --level 3 mysqld on
+	service  mysqld start
+else
+	echo "跳过安装mysql，正在执行下一步"
+fi
+#================================================================================================
+echo "是否安装nginx,如果执行输入y,否则输入任意字符串"
+read judge2
+if [ "$judge2" = "y" ] ;then
+	echo "正在安装nginx"
+	rpm -ivh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
+	yum install -y nginx
+	chkconfig --level 3 nginx on
+	service nginx start
+else
+        echo "跳过安装nginx，正在执行下一步"
+fi
+#================================================================================================
+echo "是否安装redis,如果执行输入y,否则输入任意字符串"
+read judge3
+if [ "$judge3" = "y" ] ;then
+	 rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+     yum -y --enablerepo=remi,remi-test install redis
+     
+     
+else
+        echo "跳过安装redis，程序结束"
+fi
